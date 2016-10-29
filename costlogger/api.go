6d0d7658597444e9434b921db1bee8e9f7639a1b
Paul Cadman <paul.cadman@zuhlke.com>
2016-10-29 13:36:55 +0100

    Fix references to method name

--- costlogger/api.go
+++ costlogger/api.go
@@ -9,7 +9,7 @@ import (
 const clientId = "78836484100-c7tmmk03qn4ujpk2b4ea434p4oqtomto.apps.googleusercontent.com"
 
 var (
-	scopes	  = []string{endpoints.EmailScope}
+	scopes    = []string{endpoints.EmailScope}
 	clientIds = []string{clientId, endpoints.APIExplorerClientID}
 	// in case we'll want to use TicTacToe API from an Android app
 	audiences = []string{clientId}
@@ -39,13 +39,13 @@ func (cl *CostLoggerAPI) CostList(r *http.Request,
 func RegisterService() (*endpoints.RPCService, error) {
 	api := &CostLoggerAPI{}
 	rpcService, err := endpoints.RegisterService(api,
-			"costlogger", "v1", "Cost Logger API", true)
+		"costlogger", "v1", "Cost Logger API", true)
 	if err != nil {
 		return nil, err
 	}
 
 	info := rpcService.MethodByName("CostsList").Info()
-	info.Path, info.HTTPMethod, info.Name = "costs", "GET", "costs.list"
+	info.Path, info.HTTPMethod, info.Name = "cost", "GET", "cost.list"
 	info.Scopes, info.ClientIds, info.Audiences = scopes, clientIds, audiences
 
 	return rpcService, nil
